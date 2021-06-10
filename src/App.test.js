8f92288f41a909147e19f023fd45895075ecaefd
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-06-10 19:28:45 -0300

    Iniciando escopo de teste automatizado

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+/*import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("", () => {
+  describe("", () => {
+    it("", () => {
+    
+      render(<App />);
+
+      expect(screen.getByText("")).toBeInTheDocument();// test or expect
+    });
+  });
+});*/
