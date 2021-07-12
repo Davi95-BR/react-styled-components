1606dff9b632ba44d5bafda6b540c1b5fac27578
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-07-12 18:25:11 -0300

    Treinando comandos do git

--- src/Components/Extrato/index.js
+++ src/Components/Extrato/index.js
@@ -12,7 +12,7 @@ const Extrato = () => {
         );
       })
       }
-      <Botao primary>Extrato de 10 a 30 dias</Botao>
+      <Botao primary>Extrato de 10 á 30 dias</Botao>
     </Box>
   );
 };
